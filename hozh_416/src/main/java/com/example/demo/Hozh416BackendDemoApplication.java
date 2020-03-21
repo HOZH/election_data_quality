ba17b81c0c4b900f909b672ab6a52c2e77c0d8be
HOZH <hong.zheng@stonybrook.edu>
2020-03-21 13:53:00 -0400

    loaded inital demo project to the repo

---
+++ hozh_416/src/main/java/com/example/demo/Hozh416BackendDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hozh416BackendDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hozh416BackendDemoApplication.class, args);
+	}
+
+}
