33156f4b122c10df0948a6ec6f41a00191776d3a
Hyejun <hye-jun.jeong@stonybrook.edu>
2020-05-01 23:30:20 -0400

    linked the db and the server

---
+++ hozh_416/src/test/java/com/example/demo/CountyServiceTest.java
@@ -0,0 +1,35 @@
+package com.example.demo;
+
+import com.example.demo.service.CountyService;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.hamcrest.CoreMatchers.is;
+
+/*
+ * @created 27/04/2020 - 3:15 PM
+ * @project IntelliJ IDEA
+ * @author Hong Zheng
+ */
+
+
+@ComponentScan()
+@EnableJpaRepositories()
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class CountyServiceTest {
+
+  @Autowired
+  CountyService cs;
+
+  @Test
+  public void selectCountyById() {
+    Assert.assertThat(cs.selectCountyById("cid").getId(), is("cid"));
+  }
+}
---
+++ hozh_416/src/test/java/com/example/demo/PrecinctServiceTest.java
@@ -0,0 +1,44 @@
+package com.example.demo;
+
+import com.example.demo.entity.Precinct;
+import com.example.demo.service.PrecinctService;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.hamcrest.CoreMatchers.is;
+
+// todo test will be independent to the current database later
+/*
+ * @created 27/04/2020 - 3:16 PM
+ * @project IntelliJ IDEA
+ * @author Hong Zheng
+ */
+
+@ComponentScan()
+@EnableJpaRepositories()
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class PrecinctServiceTest {
+
+  @Autowired
+  PrecinctService ps;
+
+  @Test
+  public void selectPrecinctById() {
+    Assert.assertThat(ps.selectPrecinctById("pid").getId(), is("pid"));
+  }
+
+  @Test
+  public void updatePrecinct() {
+    Precinct current = ps.selectPrecinctById("pid");
+    current.setGhost(true);
+    ps.updatePrecinct(current);
+    Assert.assertThat(ps.selectPrecinctById("pid").isGhost(), is(true));
+  }
+}
